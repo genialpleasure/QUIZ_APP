a71c55e8bda013bdce59e921d1c25d24043389d0
genialpleasure <isaacnpleasure01@gmail.com>
2025-12-11 17:26:00 +0100

    setting different timer based on selected answers and outputting quiz result

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,42 +3,57 @@ import Answers from "./Answers.jsx"
 import { useState } from "react"
 import QUESTIONS from "../questions.js"
 
-export default function Question({ index, 
-    onSelectAnswer,    
-    onSkipAnswer}) {
-        const [answer, setAnswer] =useState({
-            selectedAnswer: '',
+export default function Question({ index,
+    onSelectAnswer,
+    onSkipAnswer }) {
+    const [answer, setAnswer] = useState({
+        selectedAnswer: '',
+        isCorrect: null
+    });
+
+    let timer = 10000;
+
+    if (answer.selectedAnswer) {
+        timer = 1000;
+    }
+
+    if (answer.isCorrect !== null) {
+        timer = 2000;
+    }
+
+    function handleSelectAnswer(answer) {
+        setAnswer({
+            selectedAnswer: answer,
             isCorrect: null
-        });
+        })
 
-        function handleSelectAnswer(answer) {
-            setAnswer({ 
-                selectedAnswer: answer,
-                isCorrect: null
-            })
-
-            setTimeout(() => {
-                setAnswer({
+        setTimeout(() => {
+            setAnswer({
                 selectedAnswer: answer,
                 isCorrect: QUESTIONS[index].answers[0] === answer
-            }) 
+            })
 
             setTimeout(() => {
                 onSelectAnswer(answer);
             }, 2000);
-            }, 1000);
-        }
+        }, 1000);
+    }
 
-        let answerState = '';
+    let answerState = '';
 
-        if (answer.selectedAnswer && answer.isCorrect !== null) {
-            answerState = answer.isCorrect ? 'correct' : 'wrong';
-        } else if (answer.selectedAnswer) {
-            answerState = 'answered';
-        }
+    if (answer.selectedAnswer && answer.isCorrect !== null) {
+        answerState = answer.isCorrect ? 'correct' : 'wrong';
+    } else if (answer.selectedAnswer) {
+        answerState = 'answered';
+    }
 
     return <div id="question">
-        <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+        <QuestionTimer
+            key={timer}
+            timeout={timer}
+            onTimeout={answer.selectedAnswer == '' ? onSkipAnswer : null}
+            mode={answerState}
+        />
         <h2>{QUESTIONS[index].text}</h2>
         <Answers
             answers={QUESTIONS[index].answers}
--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
 
-export default function QuestionTimer({ timeout, onTimeout }) {
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
     const [remainingTime, setRemainingTime] = useState(timeout);
 
     useEffect(() => {
         console.log('Setting timeout');
-       const timeer = setTimeout(() => {
+       const timer = setTimeout(() => {
         onTimeout();
     }, timeout);
     return () => {
-        clearTimeout(timeer);
+        clearTimeout(timer);
     }
     }, [timeout, onTimeout]);
     
@@ -24,5 +24,5 @@ export default function QuestionTimer({ timeout, onTimeout }) {
     }, []);
 
 
-    return <progress id='question-time' max={timeout} value={remainingTime} />;
-}
+    return <progress id='question-time' max={timeout} value={remainingTime} className={mode}/>;
+} 
